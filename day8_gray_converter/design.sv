3fe852a4cfc7699fce44cf07e9e90316400136d8
minhcuong1994 <92781670+minhcuong1994@users.noreply.github.com>
2023-11-16 09:44:37 +0700

    Delete day8_gray_converter/design.sv

--- day8_gray_converter/design.sv
+++
@@ -1,9 +0,0 @@
-// Code your design here
-module gray_converter #(parameter n=4) (
-  input [n-1:0]bin,
-  output reg [n-1:0]gray);
-    
-  assign gray = {bin[n-1], bin[n-2:0] ^ bin[n-1:1]};
-
-  
-endmodule
